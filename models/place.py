53a3c45932a8036cd32faabe977ca294f922065d
EugeneJoe <jmuthui70@gmail.com>
2021-08-09 16:51:50 +0000

    Modify to include SQLAlchemy code

--- models/place.py
+++ models/place.py
@@ -1,18 +1,52 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 
 
-class Place(BaseModel):
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False)
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), primary_key=True,
+                             nullable=False))
+
+
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
     amenity_ids = []
+
+    reviews = relationship("Review", backref="place",
+                           cascade="all, delete, delete-orphan")
+    amenities = relationship("Amenity", secondary=place_amenity,
+                             back_populates="place_amenities", viewonly=False)
+
+    @property
+    def reviews(self):
+        """Returns a list of 'Review' instances for this place"""
+        my_reviews = [rev for rev in reviews where rev.place_id == self.id]
+        return my_reviews
+
+    @property
+    def amenities(self):
+        """Return a list of 'Amenity' instances for this place"""
+        my_amenities = [amen for amen in amenities where amen.id in
+                        self.amenity_ids]
+
+    @amenities.setter
+    def amenities(self, value):
+        """Add amenity id of amenity available at this place in amenity_ids"""
+        if (type(value) is Amenity):
+            amenity_ids.append(value.id)
--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+class User(BaseModel, Base):
+    """This class defines a user by various attributes
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    Attributes:
+        email (string): contains the user's email
+        password (string): contains the user's password
+        first_name (string): contains the user's first name
+        last_name (string): contains the user's last name
+    """
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete, delete-orphan")
