809657dd874d86def0255e11e96073cd90941f95
Yosef-S-A <yosefsamuel22@gmail.com>
2021-08-10 14:26:11 +0000

    updated file_storage.py, console.py script to correct there outpu and removed temp files

--- console.py
+++ console.py
@@ -227,14 +227,14 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all(args).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
-        print(print_list)
+        print(*print_list)
 
     def help_all(self):
         """ Help information for the all command """
