2bed72240c9e962268e5c8cf7559a467de6f1c1e
EugeneJoe <jmuthui70@gmail.com>
2021-08-11 22:05:33 +0000

    Add more tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,17 +5,17 @@ from models.base_model import BaseModel
 from models import storage
 import os
 
-
+unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == 'db', "Test FileStorage only")
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
     def setUp(self):
         """ Set up test environment """
         del_list = []
-        for key in storage._FileStorage__objects.keys():
+        for key in storage.all().keys():
             del_list.append(key)
         for key in del_list:
-            del storage._FileStorage__objects[key]
+            del storage.all()[key]
 
     def tearDown(self):
         """ Remove storage file at end of tests """
@@ -98,12 +98,11 @@ class test_fileStorage(unittest.TestCase):
         """ Key is properly formatted """
         new = BaseModel()
         _id = new.to_dict()['id']
-        for key in storage.all().keys():
+        for key in storage._FileStorage__objects.keys():
             temp = key
         self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
     def test_storage_var_created(self):
         """ FileStorage object storage created """
         from models.engine.file_storage import FileStorage
-        print(type(storage))
         self.assertEqual(type(storage), FileStorage)
