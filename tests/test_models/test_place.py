e1450fdc21210571753dbfaa9dcfbe3d6f1df337
EugeneJoe <jmuthui70@gmail.com>
2021-08-11 23:28:17 +0000

    Update tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """ Test the Place class """
+import pep8
+import inspect
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
 
 
 class test_Place(test_basemodel):
-    """ """
+    """ Tests for Place class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialize the class and set up some resources """
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
@@ -78,3 +81,48 @@ class test_Place(test_basemodel):
         new = self.value()
         self.assertTrue(hasattr(new, "amenity_ids"))
         self.assertEqual(type(new.amenity_ids), list)
+
+
+class TestPlaceDocs(unittest.TestCase):
+    """Tests to check the documentation and style of Place class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.place_f = inspect.getmembers(Place, inspect.isfunction)
+
+    def test_pep8_conformance_place(self):
+        """Test that models/place.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/place.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_place(self):
+        """Test that tests/test_models/test_place.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_place.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_place_module_docstring(self):
+        """Test for the place.py module docstring"""
+        self.assertIsNot(place.__doc__, None,
+                         "place.py needs a docstring")
+        self.assertTrue(len(place.__doc__) >= 1,
+                        "place.py needs a docstring")
+
+    def test_place_class_docstring(self):
+        """Test for the Place class docstring"""
+        self.assertIsNot(Place.__doc__, None,
+                         "Place class needs a docstring")
+        self.assertTrue(len(Place.__doc__) >= 1,
+                        "Place class needs a docstring")
+
+    def test_place_func_docstrings(self):
+        """Test for the presence of docstrings in Place methods"""
+        for func in self.place_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
