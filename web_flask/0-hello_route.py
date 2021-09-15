02ed850b025b496883e6a897033b7640532533ea
Yosef-S-A <yosefsamuel22@gmail.com>
2021-09-15 20:30:48 +0000

    pep8 complient

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
+
 """
 a script that starts a Flask web application
 """
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
@@ -12,4 +15,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
